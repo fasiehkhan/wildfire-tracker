af8583667ed247e9130427376728a4721d93b58b
Fasieh Khan <56199997+fasiehkhan@users.noreply.github.com>
2020-12-25 01:21:43 -0500

    added connection to API and resolved UI bug

---
+++ src/components/Map.js
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import GoogleMapReact from 'google-map-react'
+import LocationMarker from './LocationMarker'
+
+const Map = ({ eventData, center, zoom }) => {
+    
+    const markers = eventData.map((ev) => {
+        if(ev.categories[0].id === 8) {
+            return <LocationMarker lat={ev.geometries[0].coordinates[1]} 
+            lng={ev.geometries[0].coordinates[0]}/>
+        }
+        return null
+    })
+
+    return (
+        <div className="map">
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 
+                    'AIzaSyBWgy9GXhdMPj3cLlrfFkuufD0iWlgeK5M' }}
+                defaultCenter={ center }
+                defaultZoom={ zoom }
+            >
+            {markers}
+            </GoogleMapReact>
+        </div>
+    )
+}
+
+Map.defaultProps = {
+    center: {
+        lat: 42.3265,
+        lng: -122.8756
+    },
+    zoom: 6
+}
+
+export default Map
