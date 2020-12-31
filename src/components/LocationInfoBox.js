a5f2c866507af8087d9b0949a268fd984efe8e7e
Fasieh Khan <56199997+fasiehkhan@users.noreply.github.com>
2020-12-30 21:35:07 -0500

    added location marker info

--- src/components/LocationInfoBox.js
+++ src/components/LocationInfoBox.js
@@ -3,8 +3,8 @@ const LocationInfoBox = ({ info }) => {
         <div className="location-info">
             <h2>Event Location Info</h2>
             <ul>
-    <li>ID: <strong>{ info.id }</strong></li>
-    <li>TITLE: <strong>{ info.title }</strong></li>
+                <li>ID: <strong>{ info.id }</strong></li>
+                <li>TITLE: <strong>{ info.title }</strong></li>
             </ul>
         </div>
     )
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
+import LocationInfoBox from './LocationInfoBox'
 
 const Map = ({ eventData, center, zoom }) => {
-    
+    const [locationInfo, setLocationInfo] = useState(null)
+
     const markers = eventData.map((ev) => {
         if(ev.categories[0].id === 8) {
             return <LocationMarker lat={ev.geometries[0].coordinates[1]} 
-            lng={ev.geometries[0].coordinates[0]}/>
+            lng={ev.geometries[0].coordinates[0]} onClick={() => setLocationInfo({ id: ev.id,
+                 title: ev.title
+            })}/>
         }
         return null
     })
@@ -22,6 +26,7 @@ const Map = ({ eventData, center, zoom }) => {
             >
             {markers}
             </GoogleMapReact>
+            {locationInfo && <LocationInfoBox info={locationInfo} />}
         </div>
     )
 }
