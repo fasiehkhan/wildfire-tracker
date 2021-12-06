4cfea187c89f438e746a20b3c2255ef4b06402bf
Fasieh Khan <fasiehkhan2@gmail.com>
2021-12-06 03:47:55 -0500

    second commit

---
+++ Library/Mobile Documents/com~apple~CloudDocs/.Trash/cps530-final/src/Pages/Page6.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+export class Page3 extends Component {
+    render() {
+        return (
+            <div style={{marginTop:"30px", marginLeft:"30px", color:"white", }}>
+                <h4 style={{textDecoration:"underline"}}>  
+                    Credits
+                </h4>
+                <div>
+                    Frontend and Backend developer: Fasieh Khan,
+                </div>
+
+
+
+            </div>
+        )
+    }
+}
+
+export default Page3
